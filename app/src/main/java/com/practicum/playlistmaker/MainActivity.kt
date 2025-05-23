b8eb7aea7f5b61bf37521506970dfd7c98e25131
VasilyKoshlakov <KoshlakovVV@yandex.ru>
2025-05-23 09:00:47 +0300

    Sprint 8 task 2

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageSearch = findViewById<Button>(R.id.button_search)
+
+        imageSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
+        }
+        val imageMedia = findViewById<Button>(R.id.button_media)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на Медиатеку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        imageMedia.setOnClickListener(imageClickListener)
+
+        val imageSettings= findViewById<Button>(R.id.button_settings)
+
+        imageSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
