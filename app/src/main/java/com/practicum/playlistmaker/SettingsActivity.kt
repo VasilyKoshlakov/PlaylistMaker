60743694c8fff84c08adfe36e9b048827388ee98
VasilyKoshlakov <KoshlakovVV@yandex.ru>
2025-05-27 23:01:04 +0300

    Sprint 9 task 1

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val settingsBackButton = findViewById<ImageButton>(R.id.button_back_settings)
+        val settingsBackButton = findViewById<ImageView>(R.id.button_back_settings)
 
         settingsBackButton.setOnClickListener {
             finish()
